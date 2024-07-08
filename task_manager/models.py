36a5b2b8695361d71f6bdc5b04d14c581783ab04
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-07-08 11:12:00 +0300

    add db.sqlite3 to .gitignore

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Urgent', 'Urgent'),
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
+    assignees = models.ManyToManyField(Worker, related_name='tasks')
+
+    def __str__(self):
+        return (f"{self.name} "
+                f"({self.priority}, due {self.deadline}) - "
+                f"{'Completed' if self.is_completed else 'In Progress'}"
+                )
