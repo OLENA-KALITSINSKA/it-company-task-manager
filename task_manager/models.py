e0e5aae34310358ac095b7eac698aa552676d1e2
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-07-23 13:58:02 +0300

    Updated task_manager/models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -10,7 +10,30 @@ class Position(models.Model):
 
 
 class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    teams = models.ManyToManyField(Team, related_name='projects')
 
     def __str__(self):
         return self.name
@@ -19,9 +42,16 @@ class TaskType(models.Model):
 class Worker(AbstractUser):
     position = models.ForeignKey(
         Position,
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
         null=True,
         blank=True
+    )
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='members'
     )
 
     def __str__(self):
@@ -41,8 +71,22 @@ class Task(models.Model):
     deadline = models.DateField()
     is_completed = models.BooleanField(default=False)
     priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
-    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
-    assignees = models.ManyToManyField(Worker, related_name='tasks')
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    assignees = models.ManyToManyField(Worker)
+    tags = models.ManyToManyField(Tag, related_name='tasks')
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='tasks'
+    )
+
+    def formatted_deadline(self):
+        return self.deadline.strftime('%d-%m-%Y')
 
     def __str__(self):
         return (f"{self.name} "
